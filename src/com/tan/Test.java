b6dd7320145f3746b892c05aac987291c512606e
jerry <jerry_tsx@163.com>
2013-02-21 10:26:18 +0800

    just commit;

--- src/com/tan/Test.java
+++ src/com/tan/Test.java
@@ -2,6 +2,6 @@ package com.tan;
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("c* 98700000000000000");
+	System.out.println("c77777777777777770000000000");
 }
 }
